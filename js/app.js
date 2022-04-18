82c6e0998a913efe1b5c0c27fb4efe869b97bbae
Fabio Caprara <fats140701@gmail.com>
2022-04-18 12:28:05 -0400

    Funcionalidade de manter conexão implementada

--- js/app.js
+++ js/app.js
@@ -1,27 +1,41 @@
+let nome;
+
 function entrarSala(){
-    const nome = {
+    nome = {
         name: prompt("Qual é o seu nome?")
     }
     console.log(nome);
-    verificarDisponibilidadeNome(nome);
+    while(verificarDisponibilidadeNome(nome) === false){
+        nome.name = prompt("Nome indisponível! Digite outro nome.")
+        verificarDisponibilidadeNome(nome);
+    }
 }
 
 function verificarDisponibilidadeNome(nome){
     const url = 'https://mock-api.driven.com.br/api/v6/uol/participants';
     const promise = axios.post(url, nome);
     console.log(promise);
+    let disponivel;
     promise.catch(tratarErroDisponibilidade);
     promise.then(tratarSucessoDisponibilidade);
+    return disponivel;
 }
 
-function tratarErroDisponibilidade(error){
+function tratarErroDisponibilidade(){
     console.log('deu ruim');
-    return false;
+    disponivel = 'false';
 }
 
 function tratarSucessoDisponibilidade(){
     console.log('deu certo');
-    return true;
+    disponivel = 'true';
+}
+
+function manterConexao(){
+    const url = 'https://mock-api.driven.com.br/api/v6/uol/status';
+    const promise = axios.post(url, nome);
+    console.log('to on');
 }
 
 entrarSala();
+setInterval(manterConexao, 4000);
